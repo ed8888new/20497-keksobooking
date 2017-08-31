d801f947c11da50a4bf628fd190e2b2e3de9d5fb
Ed D <ed8888@rambler.ru>
2017-08-31 11:31:46 +0300

    added functions and objects

--- js/map.js
+++ js/map.js
@@ -4,14 +4,14 @@ var PATH_IMAGES = 'img/avatars/user';
 var TYPE_IMAGES = '.png';
 var NUMBER_IMAGES = [1, 8];
 
-var OFFER_TITLE = ["Большая уютная квартира", "Маленькая неуютная квартира", "Огромный прекрасный дворец", "Маленький ужасный дворец", "Красивый гостевой домик", "Некрасивый негостеприимный домик", "Уютное бунгало далеко от моря", "Неуютное бунгало по колено в воде"];
+var OFFER_TITLE = ['Большая уютная квартира', 'Маленькая неуютная квартира', 'Огромный прекрасный дворец', 'Маленький ужасный дворец', 'Красивый гостевой домик', 'Некрасивый негостеприимный домик', 'Уютное бунгало далеко от моря', 'Неуютное бунгало по колено в воде'];
 var OFFER_PRICE = [1000, 1000000];
-var OFFER_TYPE = ["flat", "house", "bungalo"];
+var OFFER_TYPE = ['flat', 'house', 'bungalo'];
 var OFFER_ROOMS = [1, 5];
 var OFFER_GUESTS = [1, 20];
-var OFFER_CHECKIN = ["12:00", "13:00", "14:00"];
-var OFFER_CHECKOUT = ["12:00", "13:00", "14:00"];
-var OFFER_FEATURES = ["wifi", "dishwasher", "parking", "washer", "elevator", "conditioner"];
+var OFFER_CHECKIN = ['12:00', '13:00', '14:00'];
+var OFFER_CHECKOUT = ['12:00', '13:00', '14:00'];
+var OFFER_FEATURES = ['wifi', 'dishwasher', 'parking', 'washer', 'elevator', 'conditioner'];
 
 var locationX = [300, 900];
 var locationY = [100, 500];
@@ -26,32 +26,30 @@ var getRandomNum = function (x) {
   return (Math.round(Math.min.apply(null, x) - 0.5 + Math.random() * (Math.max.apply(null, x) - Math.min.apply(null, x) + 1)));
 };
 
-//console.log(Math.random() * (Math.max.apply(null, OFFER_GUESTS) - Math.min.apply(null, OFFER_GUESTS)));
-//debugger;
 var announcementBox = function () {
-    var announcement = [{
-      author: {
-        avatar: PATH_IMAGES + '0' + getRandomNum(NUMBER_IMAGES) + TYPE_IMAGES,
-      },
-      offer: {
-        title: getRandomValue(OFFER_TITLE),
-        address: getRandomNum(locationX) + ', ' + getRandomNum(locationY),
-        price: getRandomNum(OFFER_PRICE),
-        type: getRandomValue(OFFER_TYPE),
-        rooms: getRandomNum(OFFER_ROOMS),
-        guests: getRandomNum(OFFER_GUESTS),
-        checkin: getRandomValue(OFFER_CHECKIN),
-        checkout: getRandomValue(OFFER_CHECKOUT),
-        features: getRandomValue(OFFER_FEATURES),
-        description: '',
-        photos: [],
-      },
-      location: {
-        x: getRandomNum(locationX),
-        y: getRandomNum(locationY)
-      }
-    }];
-    return announcement;
+  var announcement = [{
+    author: {
+      avatar: PATH_IMAGES + '0' + getRandomNum(NUMBER_IMAGES) + TYPE_IMAGES,
+    },
+    offer: {
+      title: getRandomValue(OFFER_TITLE),
+      address: getRandomNum(locationX) + ', ' + getRandomNum(locationY),
+      price: getRandomNum(OFFER_PRICE),
+      type: getRandomValue(OFFER_TYPE),
+      rooms: getRandomNum(OFFER_ROOMS),
+      guests: getRandomNum(OFFER_GUESTS),
+      checkin: getRandomValue(OFFER_CHECKIN),
+      checkout: getRandomValue(OFFER_CHECKOUT),
+      features: getRandomValue(OFFER_FEATURES),
+      description: '',
+      photos: [],
+    },
+    location: {
+      x: getRandomNum(locationX),
+      y: getRandomNum(locationY)
+    }
+  }];
+  return announcement;
 };
 
 var renderAnnouncement = function (announ) {
