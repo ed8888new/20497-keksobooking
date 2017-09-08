a118c4fc51797f36d7662ee34540f50b97ba2d3c
Ed D <ed8888@rambler.ru>
2017-09-08 14:44:41 +0300

    added checking for field

--- js/map.js
+++ js/map.js
@@ -200,3 +200,118 @@ var clickDialogClose = function () {
 };
 
 dialogClose.addEventListener('click', clickDialogClose);
+
+
+var formAnnoun = document.querySelector('.notice__form');
+
+formAnnoun.querySelector('#address').setAttribute('required', 0);
+
+formAnnoun.querySelector('#title').setAttribute('required', 0);
+formAnnoun.querySelector('#title').setAttribute('minlength', 30);
+formAnnoun.querySelector('#title').setAttribute('maxlength', 100);
+
+formAnnoun.querySelector('#price').setAttribute('required', 0);
+formAnnoun.querySelector('#price').setAttribute('type', 'number');
+formAnnoun.querySelector('#price').setAttribute('min', 0);
+formAnnoun.querySelector('#price').setAttribute('max', 1000000);
+formAnnoun.querySelector('#price').setAttribute('value', 1000);
+
+formAnnoun.setAttribute('action', 'https://1510.dump.academy/keksobooking');
+
+var selectCheckIn = document.querySelector('#timein');
+var selectCheckOut = document.querySelector('#timeout');
+
+var changeCheckInHandler = function (evt) {
+  var checkIn = evt.currentTarget.value;
+  var checkOutOptions = selectCheckOut.querySelectorAll('option');
+
+  for (var i = 0; i < checkOutOptions.length; i++) {
+    if (checkOutOptions[i].value === checkIn) {
+      checkOutOptions[i].setAttribute('selected', 0);
+    } else {
+      checkOutOptions[i].removeAttribute('selected');
+    }
+  }
+};
+
+selectCheckIn.addEventListener('change', changeCheckInHandler);
+
+var selectType = document.querySelector('#type');
+var selectPrice = document.querySelector('#price');
+
+var changeTypeHandler = function (evt) {
+  var typeFlat = evt.currentTarget.value;
+
+  switch (typeFlat) {
+    case 'bungalo':
+      selectPrice.value = 0;
+      break;
+    case 'flat':
+      selectPrice.value = 1000;
+      break;
+    case 'house':
+      selectPrice.value = 5000;
+      break;
+    case 'palace':
+      selectPrice.value = 10000;
+      break;
+  }
+};
+
+selectType.addEventListener('change', changeTypeHandler);
+
+var selectRoom = document.querySelector('#room_number');
+var selectCapacity = document.querySelector('#capacity');
+
+var CAPACITY_VARIANTS = {
+  1: ['1'],
+  2: ['1', '2'],
+  3: ['1', '2', '3'],
+  100: ['0']
+};
+
+var changeRoomHandler = function () {
+
+  var capacityOptions = selectCapacity.querySelectorAll('option');
+
+  if (capacityOptions !== null) {
+
+    [].forEach.call(capacityOptions, function (item) {
+
+      item.removeAttribute('style');
+      item.removeAttribute('selected');
+
+      item.disabled = (CAPACITY_VARIANTS[selectRoom.value].indexOf(item.value) >= 0) ? false : true;
+
+      if (!item.disabled) {
+        item.removeAttribute('style');
+
+        var isSelected = false;
+
+        [].forEach.call(capacityOptions, function (x) {
+          var active = (CAPACITY_VARIANTS[selectRoom.value].indexOf(x.value) >= 0) ? false : true;
+          if (!active && !isSelected) {
+            x.selected = true;
+            isSelected = true;
+          }
+          x.disabled = active;
+          x.hidden = active;
+        });
+      }
+    });
+  }
+};
+
+selectRoom.addEventListener('change', changeRoomHandler);
+
+var userInput = document.querySelector('#title');
+
+userInput.addEventListener('invalid', function () {
+  if (!userInput.validity.valid) {
+    if (userInput.validity.tooShort) {
+      userInput.style.borderColor = 'red';
+    }
+  } else {
+    userInput.removeAttribute('style');
+  }
+});
